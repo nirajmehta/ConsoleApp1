f3df2a25f6c4f3e78962cfe7c140dc46259add5e
nirajmehta <niraj.mehta72@gmail.com>
2024-08-10 14:33:39 +0100

    Local commit ConsoleApp1 for testing

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var json = JsonConvert.SerializeObject(new { Id = 1, Name = "One" });
+        Console.WriteLine(json);
+    }
+}
